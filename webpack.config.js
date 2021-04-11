c6e6523d63099d4bb92a11812a83362609b001e5
Tan Wang Leng <tanwangleng@outlook.com>
2021-04-11 09:25:15 +0800

    Add React

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   devtool: "inline-source-map",
   module: {
     rules: [
