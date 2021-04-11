aa28b61d6c9e5ad688249351cae0dd511285f39f
Tan Wang Leng <tanwangleng@outlook.com>
2021-04-11 09:21:02 +0800

    Add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
